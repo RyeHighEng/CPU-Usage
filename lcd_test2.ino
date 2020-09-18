216c41f52889ef2b83b6ea0e05ec553e6236cd60
RyeHighEng <52841981+RyeHighEng@users.noreply.github.com>
2020-09-18 14:17:21 -0400

    Update lcd_test2.ino
    Changed libraries to the adafruit library. Was having trouble clearing the buffer with u8g2lib.

--- lcd_test2.ino
+++ lcd_test2.ino
@@ -1,34 +1,57 @@
-#include "U8g2lib.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
-int cpu = 0;
-String s;
-U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE); 
 
-void draw(void) {
- // u8g2.clearBuffer();
-  cpu = Serial.read();
-  s = "CPU: " + String(cpu) + "%";
- // u8g2.drawStr(0,10, s);
-  //u8g2.sendBuffer();
-  u8g2.setCursor(0,10);
-  u8g2.print(s);
-  //delay(50);
+void setup() {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
 }
 
-void setup(void) {
-  u8g2.begin();
-  u8g2.setFont(u8g_font_unifont);
-}
-
-void loop(void) {
-  // picture loop
-  u8g2.firstPage();  
-  do {
-    draw();
-  } while( u8g2.nextPage() );
-
-
-  // rebuild the picture after some delay
-  delay(20);
+void loop() {
+  // put your main code here, to run repeatedly:
+    while(Serial.available() > 0){
+    //char inByte = 0;
+    char inByte[3];
+    //inByte = (char)Serial.read();
+    Serial.readBytesUntil(36,inByte,5);
+    display.clearDisplay();
+    display.setTextSize(1.5);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0,0);
+    display.println("CPU Usage: ");
+    display.print(inByte);
+    display.display();
+    delay(2000);
+  }
 }
