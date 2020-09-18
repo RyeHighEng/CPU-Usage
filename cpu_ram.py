ab5ffc969a1a77bb08c5fe66ac34890b172955b1
RyeHighEng <52841981+RyeHighEng@users.noreply.github.com>
2020-09-18 14:16:18 -0400

    Update cpu_ram.py
    Changed the baud rate and added a delay. Also send the string as ascii.

--- cpu_ram.py
+++ cpu_ram.py
@@ -1,21 +1,30 @@
 import psutil
 import serial
 import datetime
+import time
 
-ser = serial.Serial("COM3", 115200, timeout=1)
-print(ser.name)
+
+ser = serial.Serial("COM3",baudrate=9600, timeout=1) # Make sure this is 9600 or else it won't be able to read it on the arduino.
+# print(ser.name)
+
 study = True   
 while study is True:
+   
     now = datetime.datetime.now()
     curr_time = str(now.strftime("%H:%M:%S"))
-    cpu= int(psutil.cpu_percent(interval=1))
+    cpu= str(psutil.cpu_percent(interval=1)) + "% "
     # mem = psutil.virtual_memory().percent
     # psutil.virtual_memory().percent
-    ser.write(cpu)
+    ser.write(cpu.encode('ascii'))
+    # print(f"Serial Buffer: {ser.readline()}")
+    time.sleep(1)
+    ser.reset_input_buffer()
     print("CPU Usage: ", cpu, "%")
+    # time.sleep(2)
 
-    if curr_time < '22:30:00':
+    if curr_time < '23:30:00':
         study = True
     else:
         study = False
+    
 
